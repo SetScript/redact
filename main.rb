57ed2f7d178b557e16dd54c6fa63934107c52e84
SetScript <101998415+SetScript@users.noreply.github.com>
2023-10-01 12:31:44 +0300

    update

---
+++ main.rb
@@ -0,0 +1,2 @@
+require_relative "deps/deps.rb"
+require_relative "src/keydetector.rb"
--- src/keydetector.rb
+++ src/keydetector.rb
@@ -1,18 +1,24 @@
-require 'io/console'
-require 'colorize'
+# keydetector file
 
-file, text = ARGV[0], ''
-head = "|=--=| REDACT |=--=from setscript=--=|-\n"
-system('clear')
-puts head + text
+file = ARGV[0]
 
+begin
+    text = File.read(file)
+    head = "~ REDACT\n      -by setscript\n--------------------\n\n".green
+    system('clear')
+    puts head + text
+rescue TypeError
+    puts "Redact: file not found\n      - press enter for exit".red
+end
+
+begin
 loop do
   case key = STDIN.getch
   when "\u0004"
     system('clear')
-    print 'Сохранить файл? (y/n): '
+    print 'Save file? (Y/n): '.yellow
     File.write(file, text) if STDIN.gets.chomp.downcase == 'y'
-    puts 'Файл сохранен'.green
+    puts 'OK.'.green
     break
   when "\r" then text += "\n"
   when "\u007f" then text.chop!
@@ -20,4 +26,7 @@ loop do
   end
   system('clear')
   puts head + text
+end
+rescue
+    print "Redact: exit with error".yellow
 end
